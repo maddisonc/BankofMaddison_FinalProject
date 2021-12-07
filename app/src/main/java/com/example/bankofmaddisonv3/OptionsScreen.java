233af3a801b901d91ff1eda79b3f5ba093af7024
91093567 <91093567@mvusd.net>
2021-12-07 08:31:51 -0800

    12/7/21

---
+++ app/src/main/java/com/example/bankofmaddisonv3/OptionsScreen.java
@@ -0,0 +1,14 @@
+package com.example.bankofmaddisonv3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OptionsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options_screen);
+    }
+}
